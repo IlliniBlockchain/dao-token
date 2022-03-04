65fe442fc0ac62ed5efaa2628f059daffbc2798f
Alec Chen <alecchendev@gmail.com>
2022-03-04 17:51:57 -0600

    Implement transfer functions to revert

--- src/IlliniBlockchainSP22Token.sol
+++ src/IlliniBlockchainSP22Token.sol
@@ -7,5 +7,24 @@ contract IlliniBlockchainSP22Token is ERC1155 {
     function uri(uint256) public pure virtual override returns (string memory) {
         
     }
-    
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override {
+        revert();
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override {
+        revert();
+    }
 }
