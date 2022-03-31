1eacbb808640ef09381947a6af8908ecd48716fb
Alec Chen <alecchendev@gmail.com>
2022-03-30 20:46:03 -0500

    Setup governance contract with open zeppelin wizard

--- src/IlliniBlockchainGovernance.sol
+++ src/IlliniBlockchainGovernance.sol
@@ -1,7 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.6;
+import "openzeppelin-contracts/contracts/governance/Governor.sol";
+import "openzeppelin-contracts/contracts/governance/extensions/GovernorSettings.sol";
+import "openzeppelin-contracts/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "openzeppelin-contracts/contracts/governance/extensions/GovernorVotes.sol";
+import "openzeppelin-contracts/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 
-contract IlliniBlockchainGovernance {
+contract IlliniBlockchainGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
+        Governor("IlliniBlockchainGovernor")
+        GovernorSettings(0 /* 0 block */, 45818 /* 1 week */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(67)
+    {}
 
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function getVotes(address account, uint256 blockNumber)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return super.getVotes(account, blockNumber);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
 }
