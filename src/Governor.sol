8a95cae44343d533f0df4a84a91dbca7f7dc06db
Javier Nieto <javiermnieto89@gmail.com>
2022-04-13 22:06:06 -0500

    Simplify propose & add custom votingDelay option

--- src/Governor.sol
+++ src/Governor.sol
@@ -246,24 +246,40 @@ abstract contract Governor is Context, ERC165, EIP712, IGovernor, IERC721Receive
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
-        string memory description
-    ) public virtual override returns (uint256) {
+        string memory description,
+        uint64 votingDelay,
+        uint64 votingPeriod
+    ) public returns (uint256) {
         require(
             getVotes(_msgSender(), block.number - 1) >= proposalThreshold(),
             "Governor: proposer votes below proposal threshold"
         );
 
-        uint256 proposalId = hashProposal(targets, values, calldatas, keccak256(bytes(description)));
+        uint256 proposalId = hashProposal(
+            targets,
+            values,
+            calldatas,
+            keccak256(bytes(description))
+        );
 
-        require(targets.length == values.length, "Governor: invalid proposal length");
-        require(targets.length == calldatas.length, "Governor: invalid proposal length");
+        require(
+            targets.length == values.length,
+            "Governor: invalid proposal length"
+        );
+        require(
+            targets.length == calldatas.length,
+            "Governor: invalid proposal length"
+        );
         require(targets.length > 0, "Governor: empty proposal");
 
         ProposalCore storage proposal = _proposals[proposalId];
-        require(proposal.voteStart.isUnset(), "Governor: proposal already exists");
+        require(
+            proposal.voteStart.isUnset(),
+            "Governor: proposal already exists"
+        );
 
-        uint64 snapshot = block.number.toUint64() + votingDelay().toUint64();
-        uint64 deadline = snapshot + votingPeriod().toUint64();
+        uint64 snapshot = block.number.toUint64() + votingDelay;
+        uint64 deadline = snapshot + votingPeriod;
 
         proposal.voteStart.setDeadline(snapshot);
         proposal.voteEnd.setDeadline(deadline);
@@ -290,42 +306,17 @@ abstract contract Governor is Context, ERC165, EIP712, IGovernor, IERC721Receive
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
-        string memory description,
-        uint64 votingPeriod
-    ) public returns (uint256) {
-        require(
-            getVotes(_msgSender(), block.number - 1) >= proposalThreshold(),
-            "Governor: proposer votes below proposal threshold"
-        );
-
-        uint256 proposalId = hashProposal(targets, values, calldatas, keccak256(bytes(description)));
-
-        require(targets.length == values.length, "Governor: invalid proposal length");
-        require(targets.length == calldatas.length, "Governor: invalid proposal length");
-        require(targets.length > 0, "Governor: empty proposal");
-
-        ProposalCore storage proposal = _proposals[proposalId];
-        require(proposal.voteStart.isUnset(), "Governor: proposal already exists");
-
-        uint64 snapshot = block.number.toUint64() + votingDelay().toUint64();
-        uint64 deadline = snapshot + votingPeriod;
-
-        proposal.voteStart.setDeadline(snapshot);
-        proposal.voteEnd.setDeadline(deadline);
-
-        emit ProposalCreated(
-            proposalId,
-            _msgSender(),
-            targets,
-            values,
-            new string[](targets.length),
-            calldatas,
-            snapshot,
-            deadline,
-            description
-        );
-
-        return proposalId;
+        string memory description
+    ) public virtual override returns (uint256) {
+        return
+            propose(
+                targets,
+                values,
+                calldatas,
+                description,
+                votingDelay().toUint64(),
+                votingPeriod().toUint64()
+            );
     }
 
     /**
