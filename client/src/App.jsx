0ce82a3583f980d84d086b45e43c905264fdc006
Alec Chen <alecchendev@gmail.com>
2022-04-15 17:38:44 -0500

    Implement basic metamask wallet connect component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { ChakraProvider, Box, theme, Heading } from '@chakra-ui/react';
+import {useState, useEffect} from 'react';
+import { ChakraProvider, Box, theme, Heading, Button, Text } from '@chakra-ui/react';
+import MetaMaskOnboarding from '@metamask/onboarding'
+import { ethers, utils } from "ethers"
+import Wallet from './Wallet'
 
 function App() {
+
+  const [ connected, setConnected ] = useState(false);
+  const [ walletAddress, setWalletAddress ] = useState("");
+  const [ provider, setProvider ] = useState(null);
+  const [ signer, setSigner ] = useState(null);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Heading>Hello Blockchainers</Heading>
       </Box>
+      <Wallet connected={connected} setConnected={setConnected} walletAddress={walletAddress} setWalletAddress={setWalletAddress} setProvider={setProvider} setSigner={setSigner} />
     </ChakraProvider>
   );
 }
---
+++ client/src/Wallet.jsx
@@ -0,0 +1,67 @@
+import {useState, useEffect} from 'react';
+import { ChakraProvider, Box, theme, Heading, Button, Text } from '@chakra-ui/react';
+import MetaMaskOnboarding from '@metamask/onboarding'
+import { ethers, utils } from "ethers"
+
+function Wallet({ connected, setConnected, walletAddress, setWalletAddress, setProvider, setSigner }) {
+
+    const [ pendingConnect, setPendingConnect ] = useState(false);
+    const [ metaMaskInstalled, setMetaMaskInstalled ] = useState(false);
+
+    const isMetaMaskInstalled = () => {
+        const { ethereum } = window;
+        return Boolean(ethereum && ethereum.isMetaMask);
+      };
+    
+    
+      const onClickInstall = () => {
+        const onboarding = new MetaMaskOnboarding({ });
+        onboarding.startOnboarding();
+      };
+    
+      const onClickConnect = async () => {
+        try {
+          setPendingConnect(true);
+
+          const providerTemp = new ethers.providers.Web3Provider(window.ethereum);
+          setProvider(providerTemp);
+          const signerTemp = providerTemp.getSigner();
+          setSigner(signerTemp);
+          const account = await signerTemp.getAddress();
+          setWalletAddress(account);
+
+          setConnected(true);
+          setPendingConnect(false);
+    
+        } catch (error) {
+          console.error(error);
+        }
+      };
+    
+      useEffect(() => {
+        setMetaMaskInstalled(isMetaMaskInstalled());
+      }, []);
+    
+    return (
+        <Box>
+        {
+            connected
+            ?
+            <Text>{walletAddress.slice(0, 10) + "..."}</Text>
+            :
+            <Button colorScheme='blue' onClick={(metaMaskInstalled ? onClickConnect : onClickInstall)}>
+            {
+                metaMaskInstalled
+                ?
+                (pendingConnect ? "Pending" : "Connect wallet")
+                :
+                "Install MetaMask"
+            }
+            </Button>
+        }
+        </Box>
+    )
+
+}
+
+export default Wallet;
