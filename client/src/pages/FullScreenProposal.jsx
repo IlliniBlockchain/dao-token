1c9c122bf384f484602f98b0e630dd9ed42d587b
code-arman <armanr123@gmail.com>
2022-04-22 01:30:23 -0500

    Add tooltip

--- client/src/pages/FullScreenProposal.jsx
+++ client/src/pages/FullScreenProposal.jsx
@@ -9,6 +9,7 @@ import {
   Text,
   VStack,
   useMediaQuery,
+  Tooltip,
 } from '@chakra-ui/react';
 import { BsArrowLeft } from 'react-icons/bs';
 import { Navbar } from '../components/NavBar';
@@ -148,28 +149,61 @@ export const FullScreenProposal = () => {
             <Box border="2px solid grey" borderRadius="5px" p={4}>
               <Heading as="h6" size="md" mb={5}>
                 Cast your vote
+                <Tooltip label="Make sure your wallet is connected and you have not already voted on this proposal.">
+                  <Icon ml="10px" />
+                </Tooltip>
               </Heading>
               <VStack w="100%">
-                <Button
-                  w="100%"
-                  onClick={() => {
-                    sendVote(0, 0, 'A');
-                  }}
-                  isLoading={pressedOptionA}
-                  disabled={votePending}
-                >
-                  Option A
-                </Button>
-                <Button
-                  w="100%"
-                  isLoading={pressedOptionB}
-                  disabled={votePending}
-                  onClick={() => {
-                    sendVote(0, 0, 'B');
-                  }}
-                >
-                  Option B
-                </Button>
+                {connected ? (
+                  <Button
+                    w="100%"
+                    onClick={() => {
+                      sendVote(0, 0, 'A');
+                    }}
+                    isLoading={pressedOptionA}
+                    disabled={votePending}
+                  >
+                    Option A
+                  </Button>
+                ) : (
+                  <Tooltip hasArrow label="Search places">
+                    <Button
+                      w="100%"
+                      onClick={() => {
+                        sendVote(0, 0, 'A');
+                      }}
+                      isLoading={pressedOptionA}
+                      disabled
+                    >
+                      Option A
+                    </Button>
+                  </Tooltip>
+                )}
+                {connected ? (
+                  <Button
+                    w="100%"
+                    isLoading={pressedOptionB}
+                    disabled={votePending || !connected}
+                    onClick={() => {
+                      sendVote(0, 0, 'B');
+                    }}
+                  >
+                    Option B
+                  </Button>
+                ) : (
+                  <Tooltip hasArrow label="Search places">
+                    <Button
+                      w="100%"
+                      isLoading={pressedOptionB}
+                      disabled
+                      onClick={() => {
+                        sendVote(0, 0, 'B');
+                      }}
+                    >
+                      Option B
+                    </Button>
+                  </Tooltip>
+                )}
               </VStack>
             </Box>
           </Box>
