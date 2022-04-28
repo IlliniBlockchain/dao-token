ed7d035bc6b2b5a6b2e85a8c29fe8079aa4c1f52
Javier Nieto <javiermnieto89@gmail.com>
2022-04-27 20:56:29 -0500

    Added tests for governor proposal execution

--- src/test/IlliniBlockchainGovernor.t.sol
+++ src/test/IlliniBlockchainGovernor.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.6;
 import "ds-test/test.sol";
 import "./Vm.sol";
 
+import "@openzeppelin/contracts/governance/IGovernor.sol";
+import "../Governor.sol";
 import "../IlliniBlockchainSP22Token.sol";
 import "../IlliniBlockchainGovernor.sol";
 
@@ -70,5 +72,101 @@ contract IlliniBlockchainGovernorTest is DSTest {
         // Only governor should be able to update token id
         vm.startPrank(address(0x123));
         gov.updateTokenId(123);
+    }
+
+    function test_token_distribution() public {
+        // Proposal can't start from genesis block
+        vm.roll(10);
+
+        string memory description = "Token Distribution";
+
+        // Distribute initial tokens
+        for (uint256 i = 0; i < tokenOwners.length; i++) {
+            vm.prank(owner);
+            token.mint(tokenOwners[i], tokenID, 1, bytes(""));
+        }
+
+        // Transfer token ownership to governor
+        vm.prank(owner);
+        token.transferOwnership(address(gov));
+
+        // Setup new token metadata
+        IlliniBlockchainSP22Token.TokenMetadataParams memory newMeta = IlliniBlockchainSP22Token
+            .TokenMetadataParams({
+                year: 2022,
+                termId: 1 // Fall
+            });
+        uint256 newTokenID = tokenID + 1;
+
+        Governor.ProposalParams memory proposalParams = Governor
+            .ProposalParams({
+                targets: new address[](tokenOwners.length + 2),
+                values: new uint256[](tokenOwners.length + 2),
+                calldatas: new bytes[](tokenOwners.length + 2),
+                description: description,
+                votingDelay: 0,
+                votingPeriod: 100
+            });
+
+        // Setup calls for switching to new token
+        proposalParams.targets[0] = address(token);
+        proposalParams.values[0] = 0;
+        proposalParams.calldatas[0] = abi.encodeWithSignature(
+            "setTokenMetadata(uint256,(uint16,uint16))",
+            newTokenID,
+            newMeta
+        );
+
+        // Setup calls for distributing new tokens to previous owners
+        for (uint256 i = 0; i < tokenOwners.length; i++) {
+            proposalParams.targets[i + 1] = address(token);
+            proposalParams.values[i + 1] = 0;
+            proposalParams.calldatas[i + 1] = abi.encodeWithSignature(
+                "mint(address,uint256,uint256,bytes)",
+                tokenOwners[i],
+                newTokenID,
+                1,
+                bytes("")
+            );
+        }
+
+        // Switch to new token
+        proposalParams.targets[tokenOwners.length + 1] = address(gov);
+        proposalParams.values[tokenOwners.length + 1] = 0;
+        proposalParams.calldatas[tokenOwners.length + 1] = abi
+            .encodeWithSignature("updateTokenId(uint256)", newTokenID);
+
+        // Propose
+        vm.deal(tokenOwners[0], 1000);
+        vm.prank(tokenOwners[0]);
+        uint256 proposalID = gov.propose(proposalParams);
+
+        // Vote
+        vm.roll(15);
+        for (uint256 i = 0; i < tokenOwners.length; i++) {
+            vm.prank(tokenOwners[i]);
+            gov.castVote(
+                proposalID,
+                1 /* For */
+            );
+        }
+
+        // Go to proposal deadline
+        // start Block + voting delay + voting period
+        vm.roll(10 + 0 + 100 + 1);
+
+        // Execute
+        vm.prank(tokenOwners[0]);
+        gov.execute(
+            proposalParams.targets,
+            proposalParams.values,
+            proposalParams.calldatas,
+            keccak256(bytes(description))
+        );
+
+        // Check new token
+        for (uint256 i = 0; i < tokenOwners.length; i++) {
+            assertEq(1, token.balanceOf(tokenOwners[i], newTokenID));
+        }
     }
 }
